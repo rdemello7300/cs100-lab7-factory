50510b54cc357abd885143dbe7d539f91a46d11e
rdemello7300 <rdemello7300@gmail.com>
2021-02-17 12:12:06 -0800

    added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "add.hpp"
+#include "sub.hpp"
+#include "mult.hpp"
+#include "div.hpp"
+#include "pow.hpp"
+#include "op.hpp"
+
+using namespace std;
+
+int main(int argc, char** argc)
+{
+	
+
+	return 0;
+}
+
+
+
+
+
